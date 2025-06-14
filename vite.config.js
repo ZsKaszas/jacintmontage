b2fba5428ee24c0c184d8bd1d010d49168d92686
zSk <phantom.su1992@gmail.com>
2025-06-14 15:08:12 +0200

    fix: update Jekyll deployment destination and correct Vite base path format

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), svelte()],
-  base: "/jacintmontage",
+  base: "/jacintmontage/",
 });
